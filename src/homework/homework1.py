49035cf220a17291ccaba5b29ac1f91eba746547
zachdiamond000 <35587021+zachdiamond000@users.noreply.github.com>
2018-01-31 23:35:26 -0600

    Update homework1.py

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -1,23 +1,23 @@
-seconds=3800
-def get_hours_since_midnight(seconds):
-    hours = seconds // 3600
+def get_hours_since_midnight(total_seconds):
+    hours = total_seconds // 3600
     return hours
 
-hours = get_hours_since_midnight(seconds)
+total_seconds = int(input('Enter a number of seconds: '))
+hours = get_hours_since_midnight(total_seconds)
 format(hours,'02d')
 
-def get_minutes(seconds):
-    minutes = ( seconds % 3600)
+def get_minutes(total_seconds):
+    minutes = ( total_seconds % 3600)
     minutes //= 60
 
     return minutes
-minutes = get_minutes(seconds)
+minutes = get_minutes(total_seconds)
 format(minutes, '02d')
 
-def get_seconds(seconds, minutes):
-    total_seconds = (seconds % 3600) % 60
+def get_seconds(total_seconds, minutes):
+    seconds = (total_seconds % 3600) % 60
 
     return seconds
-total_seconds = get_seconds(seconds, minutes)
+seconds = get_seconds(total_seconds, minutes)
 
-print('The time since midnight is %02d:%02d:%0d' %(hours,minutes,total_seconds))
+print('The time since midnight is %02d:%02d:%0d' %(hours,minutes,seconds))
