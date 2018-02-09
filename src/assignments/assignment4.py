76997dcb2b029dc8ea859e1e6b6e60f8f9966bb5
zachdiamond000 <35587021+zachdiamond000@users.noreply.github.com>
2018-02-08 21:30:52 -0600

    Add files via upload

---
+++ src/assignments/assignment4.py
@@ -0,0 +1,29 @@
+def sample_function(value):
+    '''
+    This is just a sample function, you'll find a sample test_sample_function test case in test_assign4.py
+    DO NOT CHANGE THE CODE
+    :param value:
+    :return: return the given value
+    '''
+    return value
+
+def factorial(number):#value return function
+    '''
+    Given a number return its factorial using a for loop.
+    A factorial of 5 is 1 * 2 * 3 * 4 * 5 = 120.
+    :param number: A number greater than 0
+    :return: the factorial of a number
+    Using a for loop implement the following, the factorial function should
+    work for any number.
+    1 * 2 = 2
+    2 * 3 = 6
+    6 * 4 = 24
+    24 * 5 = 120
+
+    WRITE CODE AFTER THREE QUOTES BELOW
+    '''
+    total = 1
+    for i in range ( 1, number):
+        total = total * (i+1)
+        print(number, '\t', total)
+    return total
---
+++ src/assignments/main_assignment4.py
@@ -0,0 +1,30 @@
+from assignment4 import factorial
+
+def main():#void function
+    '''
+    Create a loop that'll run until the user doesn't type the letter 'y'
+    In the loop,
+    Capture one number from the keyboard and validate for a range between 1 and 10.
+    Call the factorial function.
+    Save the result to a variable.
+    Print the variable value.
+
+    Ask the user if they want to continue.
+
+    TO RUN YOUR PROGRAM GO TO IN PYTHON IDLE RUN->RUN MODULE.
+    IN THE PYTHON SHELL TYPE main()
+
+    DON'T ADD A RETURN STATEMENT TO THIS FUNCTION
+    '''
+
+    keep_going = 'y'
+    while keep_going == 'y':
+        x =int(input("Input a number between 1-10: "))
+        factorial(x)
+        print("Here is the factorial answer: ")
+
+        keep_going = input("Enter y to try again...")
+
+main()
+
+#another option to run the program is to call the main() function below and Run->Run Module
