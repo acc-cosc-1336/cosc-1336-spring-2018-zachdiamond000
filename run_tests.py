dd211359f9455e6c88595d6761a775e834fac1de
zachdiamond000 <35587021+zachdiamond000@users.noreply.github.com>
2018-02-15 21:31:45 -0600

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,4 @@
 import unittest
 
-from tests.homework import test_homework2
-
-suite = unittest.TestLoader().loadTestsFromModule(test_homework2)
-unittest.TextTestRunner(verbosity=2).run(suite)
+from tests.assignments import test_assign5
+    suite = unittest.TestLoader().loadTestsFromModule(test_assign5)
