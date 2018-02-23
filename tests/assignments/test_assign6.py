c9682caa98e58922d3b4de099ec87282fbee8d70
zachdiamond000 <35587021+zachdiamond000@users.noreply.github.com>
2018-02-22 21:34:02 -0600

    Add files via upload

---
+++ tests/assignments/test_assign6.py
@@ -0,0 +1,21 @@
+import unittest
+from assignment6 import get_count_A_C_G_and_T_in_string
+#write the import for function get_count_A_C_G_and_T_in_string
+
+
+class Test_Assign6(unittest.TestCase):
+
+    def test_countACGT_w_string_ATGCTTCAGAAAGGTCTTACG(self):
+        '''
+        Create a test case to test the count of As, Cs, Gs, and Ts in string ATGCTTCAGAAAGGTCTTACG
+        '''
+        self.assertEqual((6,4,5,6), get_count_A_C_G_and_T_in_string('ATGCTTCAGAAAGGTCTTACG'))
+
+    def test_count_ACGT_w_stringAGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC(self):
+        '''
+        Create a test case to test the count of As, Cs, Gs, and Ts in string
+        AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+        '''
+        self.assertEqual((20,12,17,21), get_count_A_C_G_and_T_in_string('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'))
+
+unittest.main(verbosity=3)        
