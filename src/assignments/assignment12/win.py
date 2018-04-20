4007b117e43a32ac9d3819b624d2e986ac61157a
zachdiamond000 <35587021+zachdiamond000@users.noreply.github.com>
2018-04-19 21:18:34 -0500

    Add files via upload

---
+++ src/assignments/assignment12/win.py
@@ -0,0 +1,21 @@
+from tkinter import Tk
+from converter import Converter
+from tkinter.ttk import Label
+
+class Win(Tk):
+
+    def __init__(self):
+
+        Tk.__init__(self, None, None)
+        self.wm_title('Tk')
+        self.geometry('640x480')
+        self.label1 = Label(self, text='Km: 100').grid(row=0, column=1)
+        
+
+        self.converter = Converter()
+        
+        self.label2 = Label(self, text= self.converter.get_miles_from_km(100)).grid(row=1, column=1)
+        
+
+        
+        self.mainloop()
